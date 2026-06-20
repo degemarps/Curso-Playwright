d638c339fd145523fedf362b2c17ea73ddd088ef
Degemar Pereira <degemardasilva@cartaodetodos.com>
2026-06-20 11:43:55 -0300

    sei la

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -4,7 +4,7 @@ test("Verify page title", async ({ page }) => {
   await page.goto("https://www.automationpratice.com.br/");
 
   let title:string = await page.title();
-  console.log(title);
+  console.log("Title: " + title);
 
   await expect(page).toHaveTitle("QAZANDO Shop E-Commerce");
 });
---
+++ tests/mytest2.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect} from '@playwright/test';
+
+test("Verify page URL", async ({ page }) => {
+  await page.goto("https://www.automationpratice.com.br/");
+
+  let url:string = await page.url();
+  console.log("URL: " + url);
+
+  await expect(page).toHaveURL(/automationpratice/);
+});
