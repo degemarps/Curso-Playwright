3cf39d1d9fafd527a5b7e032e769b76ac0d81bbe
Degemar Pereira <degemardasilva@cartaodetodos.com>
2026-06-27 11:15:20 -0300

    teste

--- tests/mytest2.spec.ts
+++ tests/mytest2.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test';
 
 test("Verify page URL", async ({ page }) => {
+
   await page.goto("https://www.automationpratice.com.br/");
 
   let url:string = page.url();
