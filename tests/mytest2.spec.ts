ae959c0dfdfe8bd5abe77f87f66b7d1a0a42f78f
Degemar Pereira <degemardasilva@cartaodetodos.com>
2026-06-27 10:39:22 -0300

    test auto pipeline

--- tests/mytest2.spec.ts
+++ tests/mytest2.spec.ts
@@ -3,7 +3,7 @@ import {test, expect} from '@playwright/test';
 test("Verify page URL", async ({ page }) => {
   await page.goto("https://www.automationpratice.com.br/");
 
-  let url:string = await page.url();
+  let url:string = page.url();
   console.log("URL: " + url);
 
   await expect(page).toHaveURL(/automationpratice/);
